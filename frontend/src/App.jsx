3f7b28be6a779ebc2811c7c800131c553134b757
Koffi GREBE <yvan.grebe@email.com>
2024-07-28 18:24:48 +0100

    Creation des pages et des components et des routes et des importations

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,24 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import AddProperty from "./pages/AddProperty";
+import Listing from "./pages/Listing";
+import Bookings from "./pages/Bookings";
+import Favourites from "./pages/Favourites";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/listing" element={<Listing />} />
+        <Route path="/addproperty" element={<AddProperty />} />
+        <Route path="/bookings" element={<Bookings />} />
+        <Route path="/favourites" element={<Favourites />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
