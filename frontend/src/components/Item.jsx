67cc4132ed22930b2c7c7a5aa2acf3835aab7af8
Koffi GREBE <yvan.grebe@email.com>
2024-08-24 23:44:53 +0100

    blogs.jsx et banner.jsx css

--- frontend/src/components/Item.jsx
+++ frontend/src/components/Item.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import HeartBtn from "./HeartBtn";
 import {
   MdOutlineBathtub,
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import About from "../components/About";
 import Hero from "../components/Hero";
 import Properties from "../components/Properties";
+import Blogs from "../components/Blogs";
+import bannerImg from "../assets/banner.png";
 
 const Home = () => {
   return (
@@ -8,6 +10,10 @@ const Home = () => {
       <Hero />
       <About />
       <Properties />
+      <Blogs />
+      <div className="max-padd-container py-16 overflow-x-hidden">
+        <img src={bannerImg} alt="" />
+      </div>
     </main>
   );
 };
