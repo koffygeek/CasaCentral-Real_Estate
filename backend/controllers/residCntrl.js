3ca8d1f78d20a9aedad0b926adf9200f593d0751
Koffi GREBE <yvan.grebe@email.com>
2024-09-03 01:44:52 +0100

    create residency ok

--- backend/controllers/residCntrl.js
+++ backend/controllers/residCntrl.js
@@ -16,4 +16,26 @@ export const createResidency = asyncHandler(async (req, res) => {
   } = req.body.data;
 
   console.log(req.body.data);
+  try {
+    const residency = await prisma.residency.create({
+      data: {
+        title,
+        description,
+        price,
+        address,
+        country,
+        city,
+        facilities,
+        image,
+        owner: { connect: { email: userEmail } },
+      },
+    });
+
+    res.send({ message: "Residency created successfully", residency });
+  } catch (err) {
+    if (err.code === "P2002") {
+      throw new Error("A residency with address already there");
+    }
+    throw new Error(err.message);
+  }
 });
