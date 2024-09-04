b0a30a5a36febbe404448aae465b8cf707191703
Koffi GREBE <yvan.grebe@email.com>
2024-09-04 23:41:12 +0100

    backend: bookVisit ok

--- backend/controllers/userCntrl.js
+++ backend/controllers/userCntrl.js
@@ -16,3 +16,29 @@ export const createUser = asyncHandler(async (req, res) => {
     });
   } else res.send(201).json({ message: "User already registrered" });
 });
+
+// to book a visit to resd
+export const bookVisit = asyncHandler(async (req, res) => {
+  const { email, date } = req.body;
+  const { id } = req.params;
+
+  try {
+    const alreadyBooked = await prisma.user.findUnique({
+      where: { email },
+      select: { bookedVisits: true },
+    });
+    if (alreadyBooked.bookedVisits.some((visit) => visit.id === id)) {
+      res.status(400).json({ message: "This residency already booked by you" });
+    } else {
+      await prisma.user.update({
+        where: { email: email },
+        data: {
+          bookedVisits: { push: { id, date } },
+        },
+      });
+      res.send("Your visit is booked successfully");
+    }
+  } catch (err) {
+    throw new Error(err.message);
+  }
+});
