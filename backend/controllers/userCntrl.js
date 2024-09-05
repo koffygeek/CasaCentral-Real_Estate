f4f07edc4f24d74990316c2c6e2b7d0573e94fda
Koffi GREBE <yvan.grebe@email.com>
2024-09-05 19:06:57 +0100

    remove a bookVisit

--- backend/controllers/userCntrl.js
+++ backend/controllers/userCntrl.js
@@ -1,5 +1,6 @@
 import asyncHandler from "express-async-handler";
 import { prisma } from "../config/prismaConfig.js";
+import { json } from "express";
 
 export const createUser = asyncHandler(async (req, res) => {
   console.log("Creating a user");
@@ -56,4 +57,36 @@ export const allBookings = asyncHandler(async (req, res) => {
   } catch (err) {
     throw new Error(err.message);
   }
+});
+
+export const cancelBookings = asyncHandler(async (req, res) => {
+  const { email } = req.body;
+  const { id } = req.params;
+  try {
+    const user = await prisma.user.findUnique({
+      where: { email: email },
+      select: { bookedVisits: true },
+    });
+    const index = user.bookedVisits.findIndex((visit) => visit.id === id);
+    if (index === -1) {
+      res.status(404).json({ message: "Booking not found" });
+    } else {
+      user.bookedVisits.splice(index, 1);
+      await prisma.user.update({
+        where: { email },
+        data: {
+          bookedVisits: user.bookedVisits,
+        },
+      });
+      res.send("Booking cancelled successfully");
+    }
+  } catch (err) {
+    throw new Error(err.message);
+  }
+});
+
+// to add a resd to favourite list of a user
+export const toFav = asyncHandler(async (req, res) => {
+  const { email } = req.body;
+  const { id } = req.params;
 });
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   allBookings,
   bookVisit,
+  cancelBookings,
   createUser,
 } from "../controllers/userCntrl.js";
 const router = express.Router();
@@ -9,5 +10,6 @@ const router = express.Router();
 router.post("/register", createUser);
 router.post("/bookVisit/:id", bookVisit);
 router.post("/allBookings", allBookings);
+router.post("/removeBooking/:id", cancelBookings);
 
 export { router as userRoute };
