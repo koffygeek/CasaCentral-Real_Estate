8b4cc93b62162034f8c25d0978bcecfa331a7cad
Koffi GREBE <yvan.grebe@email.com>
2024-09-12 16:12:44 +0100

    Layout.jsx: deplacement des components Header et Footer de app.jsx vers Layout.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,23 +4,22 @@ import AddProperty from "./pages/AddProperty";
 import Listing from "./pages/Listing";
 import Bookings from "./pages/Bookings";
 import Favourites from "./pages/Favourites";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 import { Suspense } from "react";
+import Layout from "./components/Layout";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Suspense>
-        <Header />
+      <Suspense fallback={<div>Loading Data...</div>}>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/listing" element={<Listing />} />
-          <Route path="/addproperty" element={<AddProperty />} />
-          <Route path="/bookings" element={<Bookings />} />
-          <Route path="/favourites" element={<Favourites />} />
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/listing" element={<Listing />} />
+            <Route path="/addproperty" element={<AddProperty />} />
+            <Route path="/bookings" element={<Bookings />} />
+            <Route path="/favourites" element={<Favourites />} />
+          </Route>
         </Routes>
-        <Footer />
       </Suspense>
     </BrowserRouter>
   );
